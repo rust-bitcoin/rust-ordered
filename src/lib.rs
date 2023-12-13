6c897debf2f828ac8dfb98cfa20e03f5b33de98d
Tobin C. Harding <me@tobin.cc>
2023-12-13 14:45:55 +1100

    Move code
    Move the `Borrow` impls above the `Deref` ones, done in preparation for
    adding `AsRef` and `AsMut`.

--- src/lib.rs
+++ src/lib.rs
@@ -108,6 +108,14 @@ impl<T> From<T> for Ordered<T> {
     fn from(inner: T) -> Self { Self(inner) }
 }
 
+impl<T> Borrow<T> for Ordered<T> {
+    fn borrow(&self) -> &T { &self.0 }
+}
+
+impl<T> BorrowMut<T> for Ordered<T> {
+    fn borrow_mut(&mut self) -> &mut T { &mut self.0 }
+}
+
 impl<T> Deref for Ordered<T> {
     type Target = T;
 
@@ -116,12 +124,4 @@ impl<T> Deref for Ordered<T> {
 
 impl<T> DerefMut for Ordered<T> {
     fn deref_mut(&mut self) -> &mut T { &mut self.0 }
-}
-
-impl<T> Borrow<T> for Ordered<T> {
-    fn borrow(&self) -> &T { &self.0 }
-}
-
-impl<T> BorrowMut<T> for Ordered<T> {
-    fn borrow_mut(&mut self) -> &mut T { &mut self.0 }
 }
